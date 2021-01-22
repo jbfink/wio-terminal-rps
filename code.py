b369a450d6cd83a126a42adfed8afe577345d4e2
jbfink <john.fink@gmail.com>
2021-01-22 17:04:41 -0500

    Wio prints its choice.

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ PRESS2 = DigitalInOut(board.BUTTON_2)
 PRESS1 = DigitalInOut(board.BUTTON_1)
 PRESS_JOYSTICK = DigitalInOut(board.SWITCH_PRESS)
 USERVALUE = ""
-choices = ["r","p","s"]
+choices = ["Rock","Paper","Scissors"]
 
 def light():
     if PRESS_JOYSTICK.value == False:
@@ -20,21 +20,25 @@ def light():
             board.DISPLAY.brightness = 1
 
 def wiochoose():
-    print(random.choice(choices))
+    wioChoice = random.choice(choices)
+    print("I choose... " + wioChoice + "!")
 
 def choose():
     if PRESS3.value == False:
         time.sleep(0.3)
         USERVALUE = "Rock"
         print(USERVALUE)
+        wiochoose()
     if PRESS2.value == False:
         time.sleep(0.3)
         USERVALUE = "Paper"
         print(USERVALUE)
+        wiochoose()
     if PRESS1.value == False:
         time.sleep(0.3)
         USERVALUE = "Scissors"
         print(USERVALUE)
+        wiochoose()
 
 #    wiochoose()
 #    the above line currently just spews out choices without waiting. will fix.
